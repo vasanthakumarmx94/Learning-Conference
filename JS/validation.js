a5d367e410bae2b23b6c8f9ef0d3b10523be478b
vasanthakumarmx94 <vasanthakumarmx94@gmail.com>
2021-09-28 09:35:03 +0530

    Validation Added commit all 2

--- JS/validation.js
+++ JS/validation.js
@@ -8,9 +8,11 @@ function validatefname(){
         spanfoot1.innerHTML="This field Required";
         fnameNode.style.border="2px solid red";
         spanfoot1.style.color="red";
+        return false;
     }
     else{
         fnameNode.style.border="2px solid green";
+        return true;
     }
 }
 
@@ -25,56 +27,28 @@ function validationemail(){
         spanfoot2.innerHTML="This field Required";
         emailNode.style.border="3px solid red";
         spanfoot2.style.color="red";
+        return false;
     }
     else if(!emailId.includes('@')||substr=='')
     {
         spanfoot2.innerHTML="Invalid EmailId";
         emailNode.style.border="2px solid red";
         spanfoot2.style.color="red";
+        return false;
     }
     else{
         emailNode.style.border="2px solid green";
+        return true;
     }
 
 }
 //let =document.getElementById("submitfornews");
 
-function submitfornews(){
-    let fnameNode=document.getElementById("firstnamenews");
-    let spanfoot1=document.getElementById('errorfoot1');
-    let emailNode=document.getElementById("emailnews");
-    let  spanfoot2=document.getElementById("errorfoot2");
-    spanfoot1.innerHTML="";
-    let firstname=fnameNode.value;
-    if(firstname=="")
-    {
-        spanfoot1.innerHTML="This field Required";
-        fnameNode.style.border="2px solid red";
-        spanfoot1.style.color="red";
-    }
-    else{
-        fnameNode.style.border="2px solid green";
-    }
-
-    spanfoot2.innerHTML='';
-    let emailId=emailNode.value;
-    let substr=emailId.substring(emailId.indexOf('@')+1);
-    if(emailId=='')
-    {
-        spanfoot2.innerHTML="This field Required";
-        emailNode.style.border="3px solid red";
-        spanfoot2.style.color="red";
-    }
-    else if(!emailId.includes('@')||substr=='')
-    {
-        spanfoot2.innerHTML="Invalid EmailId";
-        emailNode.style.border="2px solid red";
-        spanfoot2.style.color="red";
-    }
-    else{
-        emailNode.style.border="2px solid green";
-    }
-
+function validateForm(){
+    let v1=validatefname();
+    let v2=validationemail();
+    return(v1&&v2);
+    
 }
     
 
@@ -91,9 +65,11 @@ function validatefullname(){
         fullnameNode.style.border="2px solid red";
         spanNode1.style.color="red";
         spanNode1.style.fontSize="15px";
+        return false;
     }
     else{
         fullnameNode.style.border="2px solid green";
+        return true;
     }
 }
 
@@ -109,6 +85,7 @@ function validateemail(){
         remailNode.style.border="3px solid red";
         spanNode2.style.color="red";
         spanNode2.style.fontSize="15px";
+        return false;
     }
     else if(!emailId.includes('@')||substr=='')
     {
@@ -116,9 +93,11 @@ function validateemail(){
         remailNode.style.border="2px solid red";
         spanNode2.style.color="red";
         spanNode2.style.fontSize="15px";
+        return false;
     }
     else{
         remailNode.style.border="2px solid green";
+        return true;
     }
 
 }
@@ -134,15 +113,30 @@ function validatenpass(){
         npassNode.style.border="2px solid red";
         spanNode3.style.color="red";
         spanNode3.style.fontSize="15px";
+        return false;
     }
     else if(npass<=0 || npass>5){
         spanNode3.innerHTML="This field must be between 1 to 4";
         npassNode.style.border="2px solid red";
         spanNode3.style.color="red";
         spanNode3.style.fontSize="15px";
+        return false;
 
     }
     else{
         npassNode.style.border="2px solid green";
+        return true;
     }
 }
+
+
+function validateRForm(){
+    let v1=validatefullname();
+    let v2=validateemail();
+    let v3= validatenpass()
+
+    return(v1&&v2&&v3);
+    
+}
+
+
